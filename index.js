7f3d30d25115a439b1bd3cb9c84215e1c8eed92f
Akshat-Dwivedi <akshat10dwivedi@gmail.com>
2023-11-07 17:44:07 +0530

    logic update

--- index.js
+++ index.js
@@ -12,13 +12,30 @@ const mapping = {
   D: 4,
   E: 5,
 };
+
+const check = (position) => {
+  for (let i = 1; i <= 4; i++) {
+    console.log(position, arr[i]);
+    if (Number(position) === Number(arr[i])) {
+      return true;
+    }
+  }
+  return false;
+};
+
 window.submitBtn = (player) => {
   let input = getById(player).value;
   if (input.length !== "") {
-    if (Number(input) === Number(arr[0])) {
+    if (player !== "X" && Number(input) === Number(arr[0])) {
       error = true;
       console.log(`Player ${player} caught X`);
       getById("outputValue").innerHTML = `Player ${player} Caught X!`;
+    } else if (player === "X") {
+      if (check(input)) {
+        error = true;
+        console.log(`X got caught!`);
+        getById("outputValue").innerHTML = `X got caught!`;
+      }
     }
     arr[mapping[player]] = Number(input);
     console.log(arr);
